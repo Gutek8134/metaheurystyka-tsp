Fixed calculation of path difference

source/PSO/mapping.py
@@ -23,8 +23,10 @@ def random_paths(number_of_paths: int, number_of_cities: int) -> set[tuple[int, 
     return paths
 
 
-def subtract_paths(path_a: NDArray[np.uint32], path_b: NDArray[np.uint32], path_a_indexes: NDArray[np.uint32]) -> list[tuple[int, int]]:
+def subtract_paths(path_a: NDArray[np.uint32], path_b: NDArray[np.uint32], path_b_indexes: NDArray[np.uint32]) -> list[tuple[int, int]]:
     """
+    Gives path_b - path_a
+
     :params:
     For indexes, if path looked like `[1,2,0]` you'd input `[2,0,1]`
 
@@ -36,7 +38,7 @@ def subtract_paths(path_a: NDArray[np.uint32], path_b: NDArray[np.uint32], path_
     for i, element in enumerate(path_b):
         if element != path_a_copy[i]:
             # Swap the elements in copy of path_a
-            cache = path_a_indexes[path_a_copy[i]]
+            cache = path_b_indexes[path_a_copy[i]]
             path_a_copy[[i, cache]
                         ] = path_a_copy[[cache, i]]
 

source/PSO/particles.py
@@ -78,8 +78,8 @@ def PSO(nodes: NDArray | ArrayLike, initial_path: NDArray[np.uint32], population
     for index, solution in enumerate(personal_bests):
 
         temp_personal_indexes = np.zeros((number_of_cities), dtype=np.uint32)
-        for i, city_index in enumerate(solution):
-            temp_personal_indexes[city_index] = i
+        for j, city_index in enumerate(solution):
+            temp_personal_indexes[city_index] = j
 
         personal_bests_indexes[index] = temp_personal_indexes
 
@@ -106,14 +106,14 @@ def PSO(nodes: NDArray | ArrayLike, initial_path: NDArray[np.uint32], population
 
         global_best_indexes = temp_global_indexes
 
-        for i, particle_path in enumerate(particle_solutions):
+        for j, particle_path in enumerate(particle_solutions):
             # (pbest - x(t-1))
             pbest_difference: list[tuple[int, int]] = subtract_paths(
-                personal_bests[i], particle_path, personal_bests_indexes[i])
+                particle_path, personal_bests[j], personal_bests_indexes[j])
 
             # (gbest - x(t-1))
             gbest_difference: list[tuple[int, int]] = subtract_paths(
-                global_best, particle_path, global_best_indexes)
+                particle_path, global_best, global_best_indexes)
 
             for swap_operator in pbest_difference:
                 if random.random() <= alpha:
@@ -130,16 +130,16 @@ def PSO(nodes: NDArray | ArrayLike, initial_path: NDArray[np.uint32], population
             # Update personal best
             new_path_length = path_length(nodes, particle_path)
             # print(i, personal_best_lengths[i], new_path_length)
-            if personal_best_lengths[i] > new_path_length:
-                personal_bests[i] = particle_path
-                personal_best_lengths[i] = new_path_length
+            if personal_best_lengths[j] > new_path_length:
+                personal_bests[j] = particle_path
+                personal_best_lengths[j] = new_path_length
 
                 # Update indexes
                 temp_personal_indexes = np.zeros(
                     (number_of_cities), dtype=np.uint32)
-                for i, city_index in enumerate(solution):
-                    temp_personal_indexes[city_index] = i
-                personal_bests_indexes[i] = temp_personal_indexes
+                for k, city_index in enumerate(solution):
+                    temp_personal_indexes[city_index] = k
+                personal_bests_indexes[j] = temp_personal_indexes
 
         print(f"\riteration {iteration_count+1}/{max_iterations}", end="")
     print()

tests/PSO/particles_test.py
@@ -87,7 +87,7 @@ class TestPSO(unittest.TestCase):
         path.insert(0, 0)
         length = path_length(path, nodes)
         pso_path, pso_length = PSO(nodes, np.array(
-            path, dtype=np.uint32), 50, 1000, 0.4, 0.7)
+            path, dtype=np.uint32), 50, 100, 0.4, 0.7)
 
         print("Random", length, path, flush=True)
         print("PSO", pso_length, pso_path, flush=True)