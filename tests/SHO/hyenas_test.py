253e607ddfeb66355cf5a47ef7da47d7d70d3562
Gutek8134 <gutek8134@gmail.com>
2024-12-03 14:38:32 +0100

    name fix

--- tests/SHO/hyenas_test.py
+++ tests/SHO/hyenas_test.py
@@ -11,7 +11,7 @@ from source.tsp_greedy.parser import parse
 from source.tsp_greedy.tsp import tsp
 
 
-class TestPSO(unittest.TestCase):
+class TestSHO(unittest.TestCase):
 
     def test_random_instance_greedy(self):
         print()
@@ -20,14 +20,14 @@ class TestPSO(unittest.TestCase):
             instance_size, max_x=1000, max_y=1000))
 
         length, path = tsp(matrix, nodes[0])
-        pso_path, pso_length = SHO(nodes, np.array(
+        sho_path, sho_length = SHO(nodes, np.array(
             path, dtype=np.uint32), 50, 1000, 0.7, 5, blur_length=9)
 
-        self.assertEqual(len(set(pso_path)), instance_size)
+        self.assertEqual(len(set(sho_path)), instance_size)
 
         print(flush=True)
         print("Greedy", length, path)
-        print("SHO", pso_length, pso_path)
+        print("SHO", sho_length, sho_path)
 
     def test_random_instance_random(self):
         print()
@@ -39,13 +39,13 @@ class TestPSO(unittest.TestCase):
         random.shuffle(path)
         path.insert(0, 0)
         length = node_path_length(path, nodes)
-        pso_path, pso_length = SHO(nodes, np.array(
+        sho_path, sho_length = SHO(nodes, np.array(
             path, dtype=np.uint32), 50, 100, 0.7, 5)
 
-        self.assertEqual(len(set(pso_path)), instance_size)
+        self.assertEqual(len(set(sho_path)), instance_size)
 
         print("Random", length, path, flush=True)
-        print("SHO", pso_length, pso_path, flush=True)
+        print("SHO", sho_length, sho_path, flush=True)
 
 
 if __name__ == "__main__":
